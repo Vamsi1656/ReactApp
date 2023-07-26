91fff9dbb18498826678b04c4ff83da1e38ab9d8
Vamsi1656 <vamsireddy1717@gmail.com>
2023-07-26 18:27:29 +0530

    ReactApp

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Link,BrowserRouter as Router,Routes,Route } from 'react-router-dom'
+import {Link,BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import Home from './Components/Home'
 import About from './Components/About'
 import Services from './Components/Services'
@@ -9,17 +9,17 @@ import Login from './Components/Login'
 class Navbar extends Component {
   render() {
     return (
-       
       <Router>
-        <nav className='navbar navbar-dark bg-dark navbar-expand-lg '>
-           <a href='./' className='navbar-brand'>Test</a>
-           <div className='ml-auto'>
+        <nav className='navbar navbar-dark bg-light navbar-expand-lg '>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEf16tX-GP19z8oLHe1Owhsk0-H6GFFhLQUQ&usqp=CAU" width={100} alt="" />
+          <Link to='/#' className='navbar-brand' style={{color:'black',fontSize:'80'}}>Digital Technologies</Link>
+           <div className='ml-auto abc'>
                <ul className='navbar-nav'>
-                  <li className='nav-list'><Link className='nav-link' to='/home'>Home</Link></li>
-                  <li className='nav-list'><Link className='nav-link' to='/about'>About</Link></li>
-                  <li className='nav-list'><Link className='nav-link' to='/services'>Services</Link></li>
-                  <li className='nav-list'><Link className='nav-link' to='/contact'>Contact</Link></li>
-                  <li className='nav-list'><Link className='nav-link' to='/login'>Login</Link></li>
+                  <li className='nav-list'><Link className='nav-link' to='/home'style={{color:'black',fontSize:'20'}} >Home</Link></li>
+                  <li className='nav-list'><Link className='nav-link' to='/about'style={{color:'black',fontSize:'20'}}>About</Link></li>
+                  <li className='nav-list'><Link className='nav-link' to='/services'style={{color:'black',fontSize:'20'}}>Services</Link></li>
+                  <li className='nav-list'><Link className='nav-link' to='/contact'style={{color:'black',fontSize:'20'}}>Contact</Link></li>
+                  <li className='nav-list'><Link className='nav-link' to='/login'style={{color:'black',fontSize:'20'}}>Login</Link></li>
                </ul>
            </div>
         </nav>
