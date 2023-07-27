a42613d0cc9f9e7e96664f896aa0630f090cba29
Vamsi1656 <vamsireddy1717@gmail.com>
2023-07-27 16:43:11 +0530

    okie

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -11,19 +11,25 @@ import React, { Component } from 'react'
           this.setState({email:event.target.value})
          }
          passwordHandler=(event)=>{
-         
+        
           this.setState({password:event.target.value})
+         }
+         submitHandler=(event)=>{
+
+          event.preventdefault()
+          console.log(this.state)
+
          }
   render() {
     return (
       <div>
         <h1>Login Form</h1>
         <pre>{JSON.stringify(this.state)}</pre>
-        <form >
+        <form className='form-group' >
         <label>Email:</label>
-        <input type="text" onChange={this.emailHandler}  /><br /><br />
+        <input type="text" onChange={this.emailHandler} className='form-control'  /><br /><br />
         <label>Password:</label>
-        <input type="text" onChange={this.passwordHandler}  /><br /><br />
+        <input type="text" onChange={this.passwordHandler}className='form-control'  /><br /><br />
         <input type="submit" value="Login" />
         </form>
     </div>
