40354965a984625694e08d92879138f4b73ff419
Vamsi1656 <vamsireddy1717@gmail.com>
2023-07-27 12:56:29 +0530

    Okie

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
 
+
  class Login extends Component {
+         state={
+          email:"",
+          password:""
+         }
+         emailHandler=(event)=>{
+         
+          this.setState({email:event.target.value})
+         }
+         passwordHandler=(event)=>{
+         
+          this.setState({password:event.target.value})
+         }
   render() {
     return (
       <div>
-        <h1>Login Page</h1>
+        <h1>Login Form</h1>
+        <pre>{JSON.stringify(this.state)}</pre>
+        <label>Email:</label>
+        <input type="text" onChange={this.emailHandler} /><br /><br />
+        <label>Password:</label>
+        <input type="text" onChange={this.passwordHandler} /><br /><br />
+        <input type="submit" value="Login" />
     </div>
     )
   }
